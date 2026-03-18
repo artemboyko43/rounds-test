7703801f70b431c176b1026c9ff3ab2fc9b4bd5e
Artem Boyko <artemboyko43@gmail.com>
2026-03-18 10:24:47 -0400

    init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { ignores: ['**/dist/**', '**/build/**', '**/coverage/**', '**/node_modules/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+];
