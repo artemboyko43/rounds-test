2b5dde645bc7850e02e72ab3dbc23ad6c33d86a7
Artem Boyko <artemboyko43@gmail.com>
2026-03-18 19:02:24 -0400

    feat(server): periodic capture scheduler

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify';
 import { registerAppsRoutes } from './routes/apps.js';
 import { registerScreenshotsRoutes } from './routes/screenshots.js';
 import cors from '@fastify/cors';
+import { startScheduler } from './services/scheduler.js';
 
 const buildServer = () => {
   const app = Fastify({
@@ -28,6 +29,10 @@ const start = async () => {
 
   const app = buildServer();
   await app.listen({ port, host });
+
+  if (process.env.DISABLE_SCHEDULER !== 'true') {
+    await startScheduler();
+  }
 };
 
 start().catch((err) => {
